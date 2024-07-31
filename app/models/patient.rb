dbc31d0bebb81dcc7847f719fbbf47bd8a2209e2
Gabriel Chein <gabrielbcmuniz@gmail.com>
2024-07-30 22:28:06 -0300

    Modified to check availability of the room on saving the patient instance

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,18 @@
 class Patient < ApplicationRecord
   belongs_to :room, optional: true
+
+  validates :gender, inclusion: { in: %w[Male Female Undeclared] }
+
+  before_save :check_room_availability
+
+  def check_room_availability
+    if new_valid_room? || room.current_occupancy < room.capacity
+      errors.add(:base, "Friendship between these users already exists")
+    end
+  end
+
+  def new_valid_room?
+    room_changed? && !room.nil?
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,6 @@
 class Room < ApplicationRecord
   has_many :patients
 
-  before_save :check_occupancy
-
   def current_occupancy
     patients.count
   end
